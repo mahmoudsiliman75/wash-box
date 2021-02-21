54ab6b3552f7baba57589c6b2cb32aef2463c870
MahmoudSiliman <modysiliman75@gmail.com>
2021-02-22 01:20:18 +0200

    finish client  profile page

--- ClientProfile.php
+++ ClientProfile.php
@@ -2,73 +2,62 @@
 <?php include('layouts/header.php') ?>
 <!-- END:: INCLUDING HEADER -->
 
-	<!-- START:: CONTENT -->
-	<div class="kt-container  kt-container--fluid  kt-grid__item kt-grid__item--fluid">
+<!-- START:: CONTENT -->
+<div class="kt-container  kt-container--fluid  kt-grid__item kt-grid__item--fluid">
+  <div class="row">
+    <div class="col-12">
 
-    <div class="row">
-      <div class="col-12">
-
-        <div class="kt-portlet">
-
-          <div class="kt-portlet__head">
-            <div class="kt-portlet__head-label d-flex justify-content-between w-100">
-                <h3 class="kt-portlet__head-title">  الملف الشخصي </h3>
-            </div>
+      <div class="kt-portlet">
+        <div class="kt-portlet__head">
+          <div class="kt-portlet__head-label d-flex justify-content-between w-100">
+            <h3 class="kt-portlet__head-title">  الملف الشخصي </h3>
           </div>
+        </div>
 
-        <!--START: CLIENTS DATATABLE-->
-        <!-- <div class="kt-portlet__body kt-portlet__body--fit">
+        <div class="row justify-content-center my-5">
+          <div class="col-6">
+            <!-- START:: PROFILE CARD -->
+            <div class="profile-card card">
+              <div class="card-head">
+                <img src="assets/pics/bg-2.jpg" class="card-img-top" alt="...">
+                <img src="assets/pics/default.jpg" class="client-pic card-img-top" alt="...">
+              </div>
 
-          <table class="standard table table-responsive-sm" width="100%">
-            <thead class="thead-dark">
-              <tr>
-                <th>#</th>
-                <th> الاسم </th>
-                <th> رقم الهاتف </th>
-                <th> البريد الالكترونى </th>
-                <th> الرصيد </th>
-                <th> الصورة </th>
-                <th class="action">إجراء</th>
+              <div class="card-body">
+                <span class="client-financ kt-badge kt-badge--success kt-badge--lg kt-badge--rounded kt-badge--bold"> 
+                  الرصيد المتاح: 300 
+                </span>
+                <div class="row">
+                  <div class="col-12">
+                    <h4 class="card-title text-center"> محمد عيد </h4>
+                  </div>
 
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td> 
-                  <a href="ClientProfile.php" class="kt-link kt-link--state kt-link--warning"> محمد عيد </a> 
-                </td>
-                <td> 01000000001 </td>
-                <td> eid@localhost.com </td>
-                <td> 1000 </td>
-                <td> 
-                  <span class="kt-media kt-media--lg">
-                    <img src="assets/pics/default.jpg" alt="image">
-                  </span>
-                </td>
-                <td align="right">
-                  <a href="ClientDetails.php" class="kt-badge kt-badge--outline kt-badge--success" data-skin="dark" data-toggle="kt-tooltip" title="تفاصيل">
-                    <i class="la la-expand"></i>
-                  </a>
+                  <div class="col-12 col-md-6 my-3">
+                    <p>  
+                      <i class="kt-menu__link-icon la la-mobile la-2x kt-font-warning" style="font-size: 20px"></i>
+                      01000000001
+                    </p>
+                  </div>
 
-                  <a href="#" class="delete kt-badge kt-badge--outline kt-badge--danger" data-skin="dark" data-toggle="kt-tooltip" data-placement="top" title="حذف">
-                    <i class="la la-trash"></i>
-                  </a>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-
-        </div> -->
-        <!--END: CLIENTS DATATABLE-->
+                  <div class="col-12 col-md-6 my-3">
+                    <p>  
+                      <i class="kt-menu__link-icon la la-envelope-o la-2x kt-font-warning" style="font-size: 20px"></i>
+                      eid@localhost.com
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- END:: PROFILE CARD -->
+          </div>
+        </div>
 
       </div>  
     </div>
-
-	</div>
-  <!-- END:: CONTENT -->
-
+  </div>
 </div>
+<!-- END:: CONTENT -->
+
 
 <!-- START:: INCLUDING FOOTER -->
 <?php include('layouts/footer.php') ?>
